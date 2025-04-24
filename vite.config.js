32ffe02aeefed8d355bc21cbee86d44cda367565
Ethan Bourguigneau <ethan.devfs@pm.me>
2025-04-24 12:20:06 +0200

    fix pipeline v6

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
 export default defineConfig({
     plugins: [
         tailwindcss(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'src/**/*',
+                    dest: 'src'
+                }
+            ]
+        })
     ],
     base: "/shopping-list/",
     build: {
         outDir: 'dist',
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                liste: resolve(__dirname, 'liste.html'),
+            }
+        }
     }
 })
